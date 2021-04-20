140b9eacad6342d0c5402239671bcb8b2384367b
bclaw <bclaw8300@gmail.com>
2021-04-20 15:50:21 -0500

    Lesson One Assignment

---
+++ hello.js
@@ -0,0 +1,24 @@
+/*
+console.log("Hello Everyone!");
+let myFirstName = "Byron";
+console.log(myFirstName);
+console.log("Hello "+myFirstName)
+console.log("Hello World");
+let x = 20, y = 35, z = 25;
+console.log(x%y);
+*/
+
+/*let heightRule = 48;
+let minHeightRule = 36;
+let childHeight = 40;
+
+if(childHeight >= heightRule) {
+    console.log("child can ride alone")
+}
+if(childHeight < minHeightRule) {
+    console.log("Please grow longer")
+}*/
+
+
+
+
---
+++ one.js
@@ -0,0 +1,18 @@
+/*
+function name() {
+    let name = prompt("How do you spell your name?", "Yoko Ono");
+    alert("Thank you " + name + ".");
+}
+*/
+
+/*
+prompt("How do you spell your name?", "Yoko Ono");
+alert("create don't hate.");
+*/
+
+var person = prompt("How do you spell your name?", "Yoko Ono");
+
+if (person != null) {
+    alert("Thank you " + person + ". Create don't hate.");
+}
+
