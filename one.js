78f1ca7954fd00e4d1b8607aed9aa16af2ffad94
bclaw <bclaw8300@gmail.com>
2021-04-21 19:36:03 -0500

    add to js file

--- one.js
+++ one.js
@@ -10,9 +10,24 @@ prompt("How do you spell your name?", "Yoko Ono");
 alert("create don't hate.");
 */
 
+/*
 var person = prompt("How do you spell your name?", "Yoko Ono");
 
 if (person != null) {
     alert("Thank you " + person + ". Create don't hate.");
 }
+*/
+
+let username = prompt("How do you spell your name?");
+
+alert(username + ", Create don't hate");
+
+let birthYear = prompt("What year was " + username + " born?");
+
+birthYear = parseInt(birthYear);
+
+let age = 2021 - birthYear;
+
+alert(username + " is " + age + " years old.");
+
 
