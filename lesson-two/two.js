b7d1301f322d4b9202ab3105d270b08c258c6de6
bclaw <bclaw8300@gmail.com>
2021-04-26 09:17:07 -0600

    added lesson two

---
+++ lesson-two/two.js
@@ -0,0 +1,95 @@
+/* 
+let student1 = "Byron";
+let student2 = "Fouzi";
+let student3 = "Nancy";
+
+let students = ["Byron", "Fouzi", "Nancy"];
+
+let student1Age = 25;
+let student2Age = 26;
+let student3Age = 30;
+
+let studentAges = [25, 26, 30];
+
+let student = {
+    name: "Byron",
+    age: 25,
+    eyeColor: "blue",
+}
+
+student1 = {
+    name: "Fouzi",
+    age: 26,
+    eyeColor: "brown",
+}
+
+let iAmHappy = true;
+
+students = [student, student1];
+alert(students[0].name);
+
+function computeCircleArea(radius) {
+    return Math.PI * radius * radius;
+}
+
+let area = computeCircleArea(20);
+alert("The area is:" + area);
+*/
+
+/*
+var i = 1;
+while (i <= 100) {
+    console.log("The number is " + i);
+    i++;
+}
+*/
+
+// for(var i=1; i<=100; i++) {
+//     console.log("We will stop counting at 100. Current number is: " + i);
+// }
+
+// for (let z = 2; ) 
+
+// let names = ["Olivia", "Emma", "Amelia"];
+// names.length
+// let count = 0;
+// while(count < names.length) {
+//     console.log()
+// }
+
+// for (let i in names) {
+//     console.log( + names[i]);
+// }
+/*
+function getDepth() {
+    return parseFloat(prompt("please enter the depth of your home"));
+}
+
+function getWidth() {
+    return parseFloat(prompt("please enter the width of your home"));
+}
+
+function getHeight() {
+    return parseFloat(prompt("please enter the height of your home"));
+}
+
+function getSweep() {
+    return parseFloat(prompt("please enter the sweep of your home"));
+}
+
+function houseVolume(width, depth, height, sweep) {
+    return livingVolume(width, depth, height) + roofVolume(width, depth, height, sweep);
+}
+*/
+
+function getMotto() {
+    return prompt("What is your favorite motto or phrase?");
+}
+
+function getCount() {
+    return parseFloat(string("How many times would you like to see it?"));
+}
+
+function printMotto() {
+
+}
