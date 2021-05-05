0ccd3e2e589edea9476d49aaa0c9b91cad22908a
bclaw <bclaw8300@gmail.com>
2021-05-05 08:38:26 -0600

    added to do while loop

--- lesson-four/four.js
+++ lesson-four/four.js
@@ -21,14 +21,28 @@ function updateDom(value, color) {
 }
 
 function numberGuessGame(min, max) {
-    let num = randomNumber(min, max).innerHTML = "I'm thinking of a number between " + min + " and " + max
+    let message = "Try to guess my number between your low and high numbers.";
+    let num = randomNumber(min, max);
+    let guessCount = 0;
     let guess;
     do {
-        guess = parseInt(prompt("Guess Again"))
-        if(guess < num || guess > num);
-    } while(guess = num)parseInt(prompt("Good Guess"))
+        guess = parseInt(prompt(message));
+        if(guess < num) {
+            message = guess + " is too low; guess again.";
+        } else if (guess > num) {
+            message = guess + " is too high; guess again.";
+        }
+        guessCount++;
+    } while(guess != num);
+    message = "Yay!!  " + guess + "  is correct.  It took you you " + guessCount + " guess(es).";
+    updateDom(message, "green");
 }
 
 function randomNumber(min, max) { 
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+function updateDOM(value, color) {
+    document.getElementById("output").innerHTML = value;
+    document.getElementById("output").style.color = color;
 }
